c019f42b948ed856e66fac1b27f3b7c5222ee274
Sumin <reina.smh@gmail.com>
2018-07-24 23:43:03 +1200

    Create prod file, Setting dev,common and prod files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,10 +1,16 @@
 const path = require("path");
+const prodConfig = require("./webpack.prod");
+const devConfig = require("./webpack.dev");
 
-module.exports = {
+const MODE = process.env.npm_lifecycle_event;
+
+const commonConfig = {
     entry: './index.js',
-    mode: 'production',
-    output: {
-        path: path.resolve(__dirname, "output"),
-        filename: "awesome-output.js"
-    }
 }
+
+if(MODE === "dev"){
+    module.exports = Object.assign({}, commonConfig, devConfig);
+}else if(MODE === "build"){
+    module.exports = Object.assign({}, commonConfig, prodConfig);
+}
+
---
+++ webpack/webpack.prod.js
@@ -0,0 +1,9 @@
+
+const path = require("path");
+module.exports = {
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, "output"),
+        filename: "awesome-output.js"
+    }
+};
